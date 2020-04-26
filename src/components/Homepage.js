f95bf8c4a6492fc14a96463b4c5a37a6e531ba44
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-25 23:34:42 -0500

    added in-line style on div tag to center and align items

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,18 +2,22 @@ import React, { Fragment } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 
 const Homepage = () => (
-
-  <Segment inverted style={{ maxWidth: 500 }}>
-    <Form inverted style={{ maxWidth: 450 }}>
-      <Form.Input fluid label="E-mail" placeholder="E-mail" />
-      <Form.Input fluid label="Password" placeholder="Password" />
-      <Button type="submit">Sign-In</Button>
-    </Form>
-    {/* <p>
+  <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }}>
+    <Segment inverted style={{ maxWidth: 500 }} >
+      <Form inverted style={{ maxWidth: 450 }}>
+        <Form.Input label="E-mail" placeholder="E-mail" />
+        <Form.Input label="Password" placeholder="Password" />
+        <Button type="submit">Sign-In</Button>
+      </Form>
+      {/* <p>
         Don't have an account? <a href='' >Sign-Up</a>
       </p> */}
-  </Segment>
-
+    </Segment>
+  </div>
 
 
 );
