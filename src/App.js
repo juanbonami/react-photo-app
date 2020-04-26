38be0fc07bd4fcbfb5de08f61c2968a657ffdefd
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-25 23:52:40 -0500

    added comments to Router in App.js

--- src/App.js
+++ src/App.js
@@ -16,13 +16,16 @@ function App() {
 
       <Router>
         <Label>Have an account?</Label>
+        {/* links to said string */}
         <Link to="/">Sign-In</Link>
         <Label>Don't have an account?</Label>
+        {/* links to said string */}
         <Link to="/sign-up">Sign-Up</Link>
 
+        {/* sets path, exact keyword used mitigate any confusion with next path */}
         <Route exact path="/" component={Homepage} />
+        {/* sets path */}
         <Route path="/sign-up" component={Signup} />
-
       </Router>
 
     </div>
