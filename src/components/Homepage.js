b49b1eae30acba21df6f2f4718681a5768731a22
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-25 22:08:58 -0500

    Removed Form.Group and added style attribute with maxWidth of 450 to resemble the structure of Signup component so whenever you toggle between Homepage and Signup components it'll be a smooth transition

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,22 +2,17 @@ import React, { Fragment } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 
 const Homepage = () => (
-  <React.Fragment>
-    <Segment inverted>
-      <Form inverted>
-        <Form.Group widths="equal">
-          <Form.Input fluid label="E-mail" placeholder="E-mail" />
-          <Form.Input fluid label="Password" placeholder="Password" />
-        </Form.Group>
-        <Button type="submit">Sign-In</Button>
-      </Form>
-      {/* <p>
+
+  <Segment inverted>
+    <Form inverted style={{ maxWidth: 450 }}>
+      <Form.Input fluid label="E-mail" placeholder="E-mail" />
+      <Form.Input fluid label="Password" placeholder="Password" />
+      <Button type="submit">Sign-In</Button>
+    </Form>
+    {/* <p>
         Don't have an account? <a href='' >Sign-Up</a>
       </p> */}
-    </Segment>
-
-
-  </React.Fragment>
+  </Segment>
 
 
 
